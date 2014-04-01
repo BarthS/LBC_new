c1dc8e920571ff0a011e2f7106d3d6ba7de852a0
BarthS <barthelemy.saugrain@gmail.com>
2014-04-01 22:25:48 +0200

    1st commit devise inc.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LBCNew::Application.config.secret_key_base = '798e2a1be3660f0bb5e73a05f035147a9974273e41d725c0c1e29d5cb3730dcbddeb4fa587a7afad4ff8923bb97d777212d335e64b6b95764262124cb30a2036'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LBCNew::Application.config.session_store :cookie_store, key: '_LBC_new_session'
